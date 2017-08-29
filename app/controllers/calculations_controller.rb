a43fa507382de463aa7ca83187cfa2616c827cf6
Anilkumar19 <anilkumarbellamkonda91@gmail.com>
2017-08-29 22:06:12 +0530

    ui part of section

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,22 +1,19 @@
 class CalculationsController < ApplicationController
 
 	def get_values
+		@calculation = User.new
 	end
 
-	def find_odd_value
-		puts "Enter N number of elements"
-		a = gets.to_i
-		count = 0
-		puts "Enter the weight of each one"
-		anil = []
-		a.times do |g|
-			count += 1
-			puts "enter the weight of #{count} element"
-			f = gets.chomp.to_i
-			anil << f
-		end
-		var = anil.uniq
-		puts "The values of array is #{anil.each_with_index.sort}"
+	def get_sizes
+		@count = 0
+		@list = []
+		@a = params[:q].chomp.to_i
+	end
+
+	def find_odd_values
+		@array = params[:q].collect { |g| g.to_i }
+		@var = @array.uniq
 		puts "number of steps to find odd element is 1"
 	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 	devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "calculations#get_values"
+  post '/get_sizes'  => "calculations#get_sizes"
+  post '/find_odd_values'  => "calculations#find_odd_values"
   
 end
